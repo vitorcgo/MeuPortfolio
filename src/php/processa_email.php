18c164599dc5f9e8574a956e39b7a72fa1b0d813
Vitor Gomes <zbro2018@gmail.com>
2025-04-06 15:03:05 -0300

    debug

--- src/php/processa_email.php
+++ src/php/processa_email.php
@@ -3,26 +3,34 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitiza os dados
     $nome = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
     $telefone = filter_var($_POST['telefone'], FILTER_SANITIZE_STRING);
     $mensagem = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
 
+    // Validação do e-mail
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "erro";
+        echo "erro_email_invalido";
         exit;
     }
 
-    $para = "admin@vitorgomes.tech"; 
+    // Configura o e-mail
+    $para = "admin@vitorgomes.tech";
     $assunto = "Novo Contato - Formulário de Serviço";
-    $corpo = "Você recebeu um novo contato:\n\n";
-    $corpo .= "Nome: $nome\nE-mail: $email\nTelefone: $telefone\nMensagem: $mensagem\n";
-    $headers = "From: $email\r\nReply-To: $email\r\nX-Mailer: PHP/" . phpversion();
+    $corpo = "Nome: $nome\n";
+    $corpo .= "E-mail: $email\n";
+    $corpo .= "Telefone: $telefone\n";
+    $corpo .= "Mensagem: $mensagem\n";
+    $headers = "From: $email\r\nReply-To: $email\r\n";
 
+    // Tenta enviar o e-mail
     if (mail($para, $assunto, $corpo, $headers)) {
         echo "sucesso";
     } else {
-        echo "erro";
+        echo "erro_envio";
     }
+} else {
+    echo "erro_metodo_invalido";
 }
 ?>
